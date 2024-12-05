fa393499611804832b18efffede2df0155870a5e
Sebastian C <sacasadevall@gmail.com>
2024-12-04 20:29:55 -0500

    Render has lost steam, I've been saving my energy for this moment

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,5 +1,30 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://sacasadevall:4VRZor3ijm6VLviT@project-cluster.vvxou.mongodb.net/');
+dotenv.config();
+
+const MONGODB_URI = process.env.MONGODB_URI || '';
+
+console.log('MONGODB_URI:', process.env.MONGODB_URI);
+
+if (!MONGODB_URI) {
+    console.error('Error: MONGODB_URI is not defined in the environment variables.');
+    process.exit(1);
+}
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGODB_URI, {
+            ssl: true, 
+            tlsAllowInvalidCertificates: true,
+        });
+        console.log('Connected to MongoDB successfully!');
+    } catch (err) {
+        console.error('Error connecting to MongoDB:', err);
+        process.exit(1);
+    }
+};
+
+connectDB();
 
 export default mongoose.connection;
